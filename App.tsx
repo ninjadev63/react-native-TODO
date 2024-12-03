97c0bb4428c9e757a1c6aa1c69740e9fa4a4f2a8
Web dev <webdev.rds@gmail.com>
2024-12-03 18:42:22 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+import {createStore, applyMiddleware} from 'redux';
+import Todo from './component/Todo';
+import rootReducer from './reducers/rootReducer';
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+const App = () => (
+  <Provider store={store}>
+    <Todo />
+  </Provider>
+);
+export default App;
